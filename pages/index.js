e7b54c997a87bbd40d4f4d5e939b1a749fea9cf1
Blake Brown <blakebrown129@gmail.com>
2021-08-08 23:06:27 -0400

    Initial intro

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import Head from 'next/head'
+import Head from "next/head";
 
 export default function Home() {
   return (
@@ -9,42 +9,206 @@ export default function Home() {
       </Head>
 
       <main>
-        <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        <div className="container">
+          <h1 className="text-align-center">Nature Coin Whitepaper</h1>
+          <h2>Introduction</h2>
+          <p>
+            Whenever I read about blockchains, I often feel as if I'm being
+            tossed into the lecture of a monotonic professor teaching bored
+            students. Byzantine fault tolerance, consensus protocols, nakamoto
+            coefficients, there's no wonder why most of the public thinks we're
+            crazy. There's certainly a time and a place to technically explain
+            cryptocurrencies, but for Nature Coin to achieve it's full potential
+            it's paramount that we appeal to a broad audience. Thus, I hope you
+            don't mind if I'm a bit more personal and engaging throughout this
+            paper.
+          </p>
+          <p>
+            I'd like to set the stage for our project by calling attention to
+            the following trends:
+          </p>
+          <ul>
+            <li>
+              Income inequality in the United States has grown substantially
+              since the 1980's, with the ultra-rich enjoying an average annual
+              income growth of about 6% while the poorest have actually seen
+              their pre-tax income steadily decrease.
+              <sup>
+                <a href="https://ourworldindata.org/income-inequality#inequality-in-the-us-has-been-growing-substantially-in-recent-decades">
+                  1
+                </a>
+              </sup>
+            </li>
+            <li>
+              In Canada, the price ratio of an average home to median income has
+              risen 2.5x since 1976.
+              <sup>
+                <a href="https://smartcdn.prod.postmedia.digital/financialpost/wp-content/uploads/2018/05/fp0607_prime_rate.png">
+                  2
+                </a>
+              </sup>
+            </li>
+            <li>
+              Atmospheric concentrations of CO<sup>2</sup> have risen above 300
+              parts per million (ppm) for the first time in over 800,000 years.
+              In fact, we are now well over 400ppm.
+              <sup>
+                <a href="https://ourworldindata.org/co2-and-other-greenhouse-gas-emissions#atmospheric-concentrations-of-co2-continue-to-rise">
+                  3
+                </a>
+              </sup>
+            </li>
+            <li>
+              Entrepreneurship has fallen by 17% across all high-income
+              countries since 1991.
+              <sup>
+                <a href="http://ftp.iza.org/dp12602.pdf">4</a>
+              </sup>
+            </li>
+          </ul>
+          <p>
+            What do rising income inequality, housing prices, carbon emissions
+            and falling entrepreneurship rates have in common? A global economy
+            that has enabled these problems to compound. The prevailing economy
+            in most parts of the world today, is capitalism.
+          </p>
+          <p>
+            If we're going to discuss the shortcomings of capitalism, we should
+            also highlight the benefits. Capitalism has shifted power from
+            governments to corporations. In doing so, we've enabled generations
+            of productivity resulting in technological breakthroughs,
+            drastically improved standards of living, upward social mobility and
+            freedom of oppression from the state.
+          </p>
+          <p>
+            Yet it's obvious, from the trends aforementioned, that capitalism{" "}
+            <i>is not</i> a silver bullet to all of our issues. This of course
+            begs the question, is there a system in which we can enjoy the
+            upsides of capitalism without the downsides? This is the underlying
+            motivation behind Nature Coin. We'd like to eliminate climate
+            change, alleviate social inequality and assist entrepreneurs while
+            maintaining our opportunities and freedom of the current system.
+          </p>
+          <blockquote>
+            “If you permit us, we'd like to change the world.”
+            <footer>
+              <cite>- Charles Hoskinson</cite>
+            </footer>
+          </blockquote>
+          <p>To begin logically reasoning about such a system, </p>
+          <p>
+            One way to logically propose a new economic model is by following
+            our history. The earliest known form of economic organization that I
+            found during my research for Nature Coin is known as a "Palace
+            economy", in which a central administration completely controls is
+            known Feudalism flourished in Medieval Europe between the 9th and
+            15th centuries
+          </p>
+          <p>
+            I'm referring to value in the monetary sense of the word. I'll let
+            you think about it for a moment. It's tricky isn't it, to define
+            such an abstract term? I did what any millennial these days would
+            do, and I just asked Google.
+          </p>
+          <blockquote>
+            Value is the <i>regard</i> that something is held to deserve; the
+            importance, worth, or usefulness of something.
+          </blockquote>
+          <p>
+            In other words, value is completely made up! The things that you and
+            I regard as valuable could be completely different.
+          </p>
+          <p>
+            Our vision behind Nature Coin is to build financial systems that
+            ameliorate society and revitalize planet Earth.
+          </p>
+          <p>
+            We envision creating an international stock market that is 10x
+            better than any existing exchange. We are not going to do this by
+            taking existing companies on the New York stock exchange and just
+            turning them into digital assets. Rather, we want to re-think the
+            entire purpose of trading securities.
+          </p>
+          <p>
+            In addition, we propose a treasury system to fund environmental and
+            social projects on a global scale.
+          </p>
+          <h3>Capitalism</h3>
 
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+          <p>
+            This newfound faith in privatization has spread to become the global
+            economic phenomenon of the 1990s. Throughout the world, governments
+            are turning over to private managers control of everything from
+            electrical utilities to prisons, from railroads to education.
+          </p>
+          <p>What do all of these have in common? </p>
+          <p>In 1782, a extraordinary event occured. </p>
+          <p>Part 2: Nature Coin - A New Economy</p>
+          <p>Part 3: Why This Changes Everything</p>
+          <blockquote>
+            “And yet a robust movement responding to the climate crisis is not
+            emerging fast enough. Why? Why aren’t we, as a species, rising to
+            our historical moment?”
+            <footer>
+              <cite>- This Changes Everything, Naomi Klein</cite>
+            </footer>
+          </blockquote>
+          <h2>References</h2>
+          <ol>
+            <li>
+              Alex Shoumatoff is the editor of Dispatches from The Vanishing
+              World, Shoumatoff&nbsp;→, M., Schmitz, O., Schmitz, O., Langlois,
+              J., Langlois, J., . . . Kolbert, E. (n.d.). Vanishing borneo:
+              Saving one of the world's last great places. Retrieved February
+              20, 2021, from{" "}
+              <a href="https://e360.yale.edu/features/vanishing-borneo-saving-one-of-worlds-last-great-places-palm-oil">
+                https://e360.yale.edu/features/vanishing-borneo-saving-one-of-worlds-last-great-places-palm-oil
+              </a>
+            </li>
+            <li>
+              Suza 11.19.2019, W., Cataneo 11.04.2020, E., &amp; Farah
+              03.27.2020, T. (2019, November 19). As tropical RAINFORESTS
+              disappear, so do potential Medicine Reservoirs. Retrieved February
+              20, 2021, from{" "}
+              <a href="https://undark.org/2019/11/19/tropical-rainforest-disappear-potential-medicine-reservoirs/">
+                https://undark.org/2019/11/19/tropical-rainforest-disappear-potential-medicine-reservoirs/
+              </a>
+            </li>
+            <li>
+              GD. Shanks, M., M. Mishra, V., MA. Phillips, J., J. Okombo, K.,
+              EA. Ashley, A., AE. Williamson, P., . . . MR. Luth, P. (1970,
+              January 01). The past, present and future of anti-malarial
+              medicines. Retrieved February 20, 2021, from{" "}
+              <a href="https://malariajournal.biomedcentral.com/articles/10.1186/s12936-019-2724-z">
+                https://malariajournal.biomedcentral.com/articles/10.1186/s12936-019-2724-z
+              </a>
+            </li>
+            <li>
+              Prakash, A., &amp; Baskaran, R. (2018, September). Acerola, an
+              untapped FUNCTIONAL Superfruit: A review on LATEST FRONTIERS.
+              Retrieved February 20, 2021, from{" "}
+              <a href="https://www.ncbi.nlm.nih.gov/pmc/articles/PMC6098779/">
+                https://www.ncbi.nlm.nih.gov/pmc/articles/PMC6098779/
+              </a>
+            </li>
+            <li>
+              Gavamukulya, Y., Abou-Elella, F., Wamunyokoli, F., &amp;
+              AEl-Shemy, H. (2014, October 11). Phytochemical screening,
+              ANTI-OXIDANT activity and in vitro Anticancer potential OF
+              Ethanolic and water leaves extracts OF annona muricata (Graviola).
+              Retrieved February 20, 2021, from{" "}
+              <a href="https://www.sciencedirect.com/science/article/pii/S1995764514602583">
+                https://www.sciencedirect.com/science/article/pii/S1995764514602583
+              </a>
+            </li>
+            <li>
+              Christy, B. (2021, February 10). Inside the dark world of the
+              rhino horn trade. Retrieved February 20, 2021, from{" "}
+              <a href="https://www.nationalgeographic.com/magazine/article/dark-world-of-the-rhino-horn-trade">
+                ttps://www.nationalgeographic.com/magazine/article/dark-world-of-the-rhino-horn-trade
+              </a>
+            </li>
+          </ol>
         </div>
       </main>
 
@@ -54,21 +218,45 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by{" "}
           <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
         </a>
       </footer>
 
       <style jsx>{`
-        .container {
-          min-height: 100vh;
-          padding: 0 0.5rem;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
+        blockquote {
+          font-size: 36px;
+          text-align: center;
+          color: #f96f5d;
+          line-height: 48px;
         }
-
+        .container {
+          width: 66.6%;
+          margin: 0 auto;
+          min-height: 100vh;
+        }
+        p {
+          font-size: 16px;
+          line-height: 28px;
+          color: #222;
+        }
+        ul,
+        ol {
+          font-size: 16px;
+          line-height: 28px;
+        }
+        a {
+          color: #f96f5d;
+          text-decoration: none;
+          -webkit-transition: opacity 0.3s ease;
+          -moz-transition: opacity 0.3s ease;
+          -ms-transition: opacity 0.3s ease;
+          -o-transition: opacity 0.3s ease;
+          transition: opacity 0.3s ease;
+        }
+        sup {
+          line-height: 0;
+        }
         main {
           padding: 5rem 0;
           flex: 1;
@@ -95,11 +283,6 @@ export default function Home() {
           display: flex;
           justify-content: center;
           align-items: center;
-        }
-
-        a {
-          color: inherit;
-          text-decoration: none;
         }
 
         .title a {
@@ -198,6 +381,11 @@ export default function Home() {
           font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
             Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
             sans-serif;
+          color: #222;
+        }
+
+        .text-align-center {
+          text-align: center;
         }
 
         * {
@@ -205,5 +393,5 @@ export default function Home() {
         }
       `}</style>
     </div>
-  )
+  );
 }
